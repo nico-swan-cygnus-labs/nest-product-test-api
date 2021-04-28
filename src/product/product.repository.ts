164b3b3e3bdda2fe1c8f6a3097a383369c7cf9f2
Nico Swan (Group) <nico.swan@global.ntt>
2021-04-28 10:57:57 +0200

    Initial

---
+++ src/product/product.repository.ts
@@ -0,0 +1,34 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateProductDTO } from './dto/create-product.dto';
+import { Product } from './product.entity';
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+  public async createProduct(
+    createProductDto: CreateProductDTO,
+  ): Promise<Product> {
+    const { name, description, price } = createProductDto;
+
+    const product = new Product();
+    product.name = name;
+    product.description = description;
+    product.price = price;
+
+    await product.save();
+    return product;
+  }
+
+  public async editProduct(
+    createProductDto: CreateProductDTO,
+    editedProduct: Product,
+  ): Promise<Product> {
+    const { name, description, price } = createProductDto;
+
+    editedProduct.name = name;
+    editedProduct.description = description;
+    editedProduct.price = price;
+    await editedProduct.save();
+
+    return editedProduct;
+  }
+}
